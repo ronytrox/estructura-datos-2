e47a6184e6014ba631051173d8b9fd5be4158e0e
ronytrox <102933405+ronytrox@users.noreply.github.com>
2025-06-02 13:23:03 -0600

    feat: Creacion de la clase para el nodo del arbol y añadidas las funciones para búsqueda y inserción

---
+++ Practica1/src/main/java/org/example/Functions/Funciones.java
@@ -0,0 +1,45 @@
+package org.example.Functions;
+
+import org.example.Nodes.TreeNode;
+
+public class Funciones {
+
+    private static TreeNode root;
+
+    public Funciones(){
+        this.root = null;
+    }
+
+    public static void addLeaf(int value) {
+        root = addLeafRec(root, value);
+    }
+
+    public static TreeNode addLeafRec(TreeNode root, int value) {
+        if (root == null) {
+            return new TreeNode(value);
+        }
+        if (value < root.getValue()) {
+            root.setLeft(addLeafRec(root.getLeft(), value));
+        } else if (value > root.getValue()) {
+            root.setRight(addLeafRec(root.getRight(), value));
+        }
+        return root;
+    }
+
+    public int search(int value) {
+        return searchRec(root, value);
+    }
+
+    public int searchRec(TreeNode root, int value) {
+        if (root == null) {
+            return -1; // Valor no encontrado
+        }
+        if (value < root.getValue()) {
+            return searchRec(root.getLeft(), value);
+        } else if (value > root.getValue()) {
+            return searchRec(root.getRight(), value);
+        } else {
+            return root.getValue(); // Valor encontrado
+        }
+    }
+}
---
+++ Practica1/src/main/java/org/example/Nodes/TreeNode.java
@@ -0,0 +1,40 @@
+package org.example.Nodes;
+
+public class TreeNode {
+
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int value) {
+
+        this.value = value;
+        this.left = null;
+        this.right = null;
+
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+}
