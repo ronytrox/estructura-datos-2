a05d4d514bed3c19ea5fbbe122dd377fa94f208b
ronytrox <102933405+ronytrox@users.noreply.github.com>
2025-06-02 13:36:35 -0600

    feat: se crea la conexión entre main y el menú, y se cargan unos ejemplos para probar el árbol

--- Practica1/src/main/java/org/example/GUI/Menu.java
+++ Practica1/src/main/java/org/example/GUI/Menu.java
@@ -1,4 +1,23 @@
 package org.example.GUI;
 
+import org.example.Functions.Funciones;
+
 public class Menu {
+    public static void displayMenu() {
+        println("Bienvenido a la inserción de datos en una estructura de árbol binario.");
+        Funciones.addLeaf(4);
+        Funciones.addLeaf(8);
+        Funciones.addLeaf(12);
+        Funciones.addLeaf(2);
+
+    }
+    
+    
+    private static void println(String message) {
+        System.out.println(message);
+    }
+    
+    private static void print(String message) {
+        System.out.print(message);
+    }
 }
